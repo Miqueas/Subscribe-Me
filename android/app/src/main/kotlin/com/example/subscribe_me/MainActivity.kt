6de1507307ad01e07c32febaf1dcbc393ff9f130
Miqueas <miqueas2020@yahoo.com>
2024-06-02 13:56:11 -0500

    Updated app id per platform

--- android/app/src/main/kotlin/com/example/subscribe_me/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/subscribe_me/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.subscribe_me
+package io.github.Miqueas.SubscribeMe
 
 import io.flutter.embedding.android.FlutterActivity
 
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"subscribe_me", origin, size)) {
+  if (!window.Create(L"io.github.Miqueas.SubscribeMe", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
