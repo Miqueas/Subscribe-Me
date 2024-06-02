e42327633ae6770b096192a54898fef8d32a998c
Miqueas <miqueas2020@yahoo.com>
2024-06-02 13:57:57 -0500

    Created banner widget atoms: icon and image

---
+++ lib/widgets/banner/atoms/icon.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+final class BannerIcon extends StatelessWidget {
+  const BannerIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: const Color(0x09FFFFFF)),
+      child: const Icon(Icons.add));
+  }
+}
---
+++ lib/widgets/banner/atoms/image.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+import "package:flutter/material.dart";
+
+final class BannerImage extends StatelessWidget {
+  const BannerImage({super.key, required this.path});
+
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+      child: Image.file(File(path), fit: BoxFit.fitWidth),
+    );
+  }
+}
