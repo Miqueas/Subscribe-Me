aea5ac3616c7af74bcb43496fa18bd6f8ba2f88e
Miqueas <miqueas2020@yahoo.com>
2024-06-03 16:02:09 -0500

    Finished subscriber guidelines and welcome message

--- lib/widgets/subscriber_guidelines/molecules/subscriber_guidelines_molecule.dart
+++ lib/widgets/subscriber_guidelines/molecules/subscriber_guidelines_molecule.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "package:subscribe_me/controllers/subscriber_guidelines_controller.dart";
+import "package:subscribe_me/controllers/subscription_controller.dart";
 import "package:subscribe_me/widgets/common/atoms/label_atom.dart";
 import "package:subscribe_me/widgets/common/atoms/input_atom.dart";
 
@@ -7,10 +10,17 @@ class SubscriberGuidelinesMolecule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final subscriptionProvider = Provider.of<SubscriptionProvider>(context);
+    final provider = Provider.of<SubscriberGuidelinesProvider>(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const LabelAtom(text: "Subscriber guidelines", padding: 40),
-        InputAtom(controller: TextEditingController())]);
+        InputAtom(
+          controller: provider.controller,
+          onChanged: (text) {
+            provider.setGuidelines(text);
+            subscriptionProvider.setGuidelines(provider.guidelines);})]);
   }
 }
--- lib/widgets/welcome_message/molecules/welcome_message_molecule.dart
+++ lib/widgets/welcome_message/molecules/welcome_message_molecule.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
+import "package:subscribe_me/controllers/subscription_controller.dart";
 import "package:subscribe_me/controllers/welcome_message_controller.dart";
 import "package:subscribe_me/widgets/common/atoms/input_atom.dart";
 import "package:subscribe_me/widgets/common/atoms/label_atom.dart";
@@ -9,12 +10,17 @@ class WelcomeMessageMolecule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final subscriptionProvider = Provider.of<SubscriptionProvider>(context);
     final provider = Provider.of<WelcomeMessageProvider>(context);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const LabelAtom(text: "Welcome message", padding: 30),
-        InputAtom(controller: provider.controller)]);
+        InputAtom(
+          controller: provider.controller,
+          onChanged: (text) {
+            provider.setMessage(text);
+            subscriptionProvider.setWelcomeMessage(provider.message);})]);
   }
 }
