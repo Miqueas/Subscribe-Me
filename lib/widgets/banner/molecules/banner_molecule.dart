9c8d11606f0b1fcccc9dfc0fb0b00e2683f82808
Miqueas <miqueas2020@yahoo.com>
2024-06-03 08:36:04 -0500

    Minor changes in banner molecule

--- lib/widgets/banner/molecules/banner_molecule.dart
+++ lib/widgets/banner/molecules/banner_molecule.dart
@@ -3,17 +3,21 @@ import "package:flutter/material.dart";
 import "package:image_picker/image_picker.dart";
 import "package:provider/provider.dart";
 import "package:subscribe_me/controllers/banner_controller.dart";
+import "package:subscribe_me/controllers/subscription_controller.dart";
 import "package:subscribe_me/widgets/banner/atoms/icon.dart";
 import "package:subscribe_me/widgets/banner/atoms/image.dart";
-import "package:subscribe_me/widgets/common/atoms/label.dart";
+import "package:subscribe_me/widgets/common/atoms/label_atom.dart";
 
 final class BannerMolecule extends StatelessWidget {
   const BannerMolecule({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final subProvider = Provider.of<SubscriptionProvider>(context);
     final provider = Provider.of<BannerProvider>(context);
     final children = <Widget>[ const BannerIcon()];
+
+    subProvider.setBanner(provider.banner);
 
     if (provider.banner.filePath != "") {
       children.add(BannerImage(path: provider.banner.filePath));
@@ -22,7 +26,7 @@ final class BannerMolecule extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Label(text: "Upload a \"welcome\" picture (optional). Press and hold the image to delete it.",
+        const LabelAtom(text: "Upload a \"welcome\" picture (optional). Press and hold the image to delete it.",
           padding: 20),
         Padding(
           padding: const EdgeInsets.only(top: 10),
@@ -36,24 +40,32 @@ final class BannerMolecule extends StatelessWidget {
                   return;
                 }
 
-                showModalBottomSheet(context: context, builder: (context) {
-                  return FittedBox(child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(children: [
-                        const Text("Select banner source", style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18)),
-                        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                          Column(children: [
-                            IconButton(onPressed: () => provider.selectBanner(ImageSource.camera),
-                              icon: const Icon(Icons.camera),
-                              iconSize: 36),
-                            const Text("Camera")]),
-                          const Padding(padding: EdgeInsets.all(10)),
-                          Column(children: [
-                            IconButton(onPressed: () => provider.selectBanner(ImageSource.gallery),
-                              icon: const Icon(Icons.photo),
-                              iconSize: 36),
-                            const Text("Gallery")])])])));});
+                showModalBottomSheet(backgroundColor: const Color(0x00FFFFFF), context: context, builder: (context) {
+                  return FittedBox(child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color.fromARGB(255, 34, 2, 39),
+                          Color.fromARGB(255, 15, 0, 19)])),
+                    padding: const EdgeInsets.all(20),
+                    child: Column(children: [
+                      const Text("Select banner source", style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18)),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                        Column(children: [
+                          IconButton(onPressed: () => provider.selectBanner(ImageSource.camera),
+                            icon: const Icon(Icons.camera),
+                            iconSize: 36),
+                          const Text("Camera")]),
+                        const Padding(padding: EdgeInsets.all(10)),
+                        Column(children: [
+                          IconButton(onPressed: () => provider.selectBanner(ImageSource.gallery),
+                            icon: const Icon(Icons.photo),
+                            iconSize: 36),
+                          const Text("Gallery")])])])));});
               }, child: Stack(children: children))))]))]);
   }
 }
